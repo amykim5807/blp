6ead33cc45c488667951b61f7cf58eaf6ee474a9
Amy <kimam@utschools.ca>
2018-08-20 16:43:20 -0400

    Reorganized files 	python/

--- equationtosolveforprice.m
+++
@@ -1,5 +0,0 @@
-function output=equationtosolveforprice(price,X,betatrue,mu,NS,cdindex,cdid,mc,weights)
-[individshares,outshare]=simulateMarketShares([X,price]*betatrue,mu,NS,cdindex);
-marketshares=sum(repmat(weights,size(price,1),1).*individshares,2);
-output=price-mc+1./(betatrue(end)*(1-marketshares));
-end
--- random_test.0py.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun 29 14:18:36 2018
-
-@author: L1YAK01
-"""
-
